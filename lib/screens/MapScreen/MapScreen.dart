057a76f2d15659fa3b1c92da47258763ffab350d
oacikel <41515954+oacikel@users.noreply.github.com>
2020-09-22 20:50:39 +0300

    Added Test Markers

--- lib/screens/MapScreen/MapScreen.dart
+++ lib/screens/MapScreen/MapScreen.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -13,12 +14,27 @@ class MapScreen extends StatefulWidget {
 
 class _MapScreenState extends State<MapScreen> {
   GoogleMapController mapController;
-
+  List<Marker> allMarkers = [];
   final LatLng _center = const LatLng(45.521563, -122.677433);
+
+
+  @override
+  void initState() {
+    allMarkers.add(Marker(
+      markerId: MarkerId("Test Marker"),
+      draggable: false,
+      onTap:(){
+        print("Marker tapped");
+      },
+      position: _center
+    ));
+  }
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
   }
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -27,6 +43,7 @@ class _MapScreenState extends State<MapScreen> {
       ),
       body: GoogleMap(
       onMapCreated: _onMapCreated,
+    markers: Set.from(allMarkers),
     initialCameraPosition: CameraPosition(
     target: _center,
     zoom: 11.0,
@@ -35,4 +52,8 @@ class _MapScreenState extends State<MapScreen> {
     );
   }
 
+
+
 }
+
+
