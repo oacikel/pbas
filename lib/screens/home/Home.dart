0f157e352e49921c12cc5d74867421c45da6a41b
oacikel <41515954+oacikel@users.noreply.github.com>
2020-10-15 08:41:51 +0300

    fixed  bug

--- lib/screens/home/Home.dart
+++ lib/screens/home/Home.dart
@@ -2,6 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
+import 'package:pbas/screens/Home/PostList.dart';
+import 'package:pbas/screens/Home/PostTile.dart';
 import 'package:pbas/services/Database.dart';
 import 'package:provider/provider.dart';
 import 'package:pbas/screens/home/PostList.dart';
@@ -17,25 +19,47 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final String LOG_TAG = "OCULCAN - HOME: ";
+  DatabaseService databaseService=new DatabaseService();
+  List<Post>posts;
+
+
+  @override
+  void initState() {
+    databaseService.posts.listen((postList) {
+      posts=postList;
+      setState(() {});
+      postList.forEach((post) {
+        databaseService.updatePostWithUser(post)
+            .whenComplete(() => setState((){debugPrint(LOG_TAG+"Updated a new story");}));
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return StreamProvider<List<Post>>.value(
-      value: DatabaseService().posts,
-      child: Scaffold(
-        body: Padding(
-          padding: const EdgeInsets.only(top: CONSTANTS.paddingAppBar),
-          child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[Container(
-                  height: MediaQuery
-                      .of(context)
-                      .size
-                      .height / 2.0,
-                  child: PostList())]),
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.only(top: CONSTANTS.paddingAppBar),
+        child: Container(
+            height: MediaQuery
+                .of(context)
+                .size
+                .height / 2.0,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: posts.length,
+                itemBuilder: (context,index){
+                return Container(
+                  height: 160,
+                  child: PostTile(
+                    post: posts[index],
+                  ),
+                );
+        })
         ),
-        
       ),
+
     );
   }
 
